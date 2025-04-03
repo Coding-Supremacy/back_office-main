1896629ab2e5434965c32063b475a1ee413ab375
marurun66 <marurun66@gmail.com>
2025-04-03 17:48:45 +0900

    리전

--- main/project_2/ui_mini2/mini2_prediction_region.py
+++ main/project_2/ui_mini2/mini2_prediction_region.py
@@ -94,21 +94,16 @@ def fetch_news(query, display=5):
 
 def run_prediction_region():
     BASE_DIR = Path(__file__).resolve().parent.parent # Path 객체 반환
-    DATA_DIR = BASE_DIR / "data_mini2"
-    MODEL_DIR = BASE_DIR / "models_mini2"
+    DATA_DIR = BASE_DIR / "mini2_data"
+    MODEL_DIR = BASE_DIR / "mini2_models"
     
     CSV_PATH_MAP = {
         "현대": DATA_DIR / "현대_시장구분별_수출실적.csv",
         "기아": DATA_DIR / "기아_시장구분별_수출실적.csv"
     }
 
-    channel = option_menu(None, ["현대", "기아"], default_index=0, orientation="horizontal",
-        icons=["car-front-fill", "truck-front-fill"],
-        styles={"container": {"padding": "0!important", "background-color": "#f9f9f9"},
-                "icon": {"color": "#2E86C1", "font-size": "20px"},
-                "nav-link": {"font-size": "16px", "text-align": "center", "margin": "0px", "padding": "10px"},
-                "nav-link-selected": {"background-color": "#2E86C1", "color": "white"}})
-
+    channel = st.session_state.get("brand", "현대")
+    
     market_label_map = {
         "미국": "북미-미국", "캐나다": "북미-캐나다", "멕시코": "북미-멕시코",
         "동유럽": "동유럽", "서유럽": "서유럽", "아시아": "아시아",
@@ -278,7 +273,7 @@ def run_prediction_region():
                 pdf.add_page()
                 pdf.set_auto_page_break(auto=True, margin=15)
 
-                FONT_PATH_ORIG = "main/project_2/fonts/NanumGothic.ttf"
+                FONT_PATH_ORIG = "main_project/project_2/fonts/NanumGothic.ttf"
                 TEMP_FONT_PATH = os.path.join(tempfile.gettempdir(), "NanumGothic.ttf")
 
                 # 임시 경로에 복사
