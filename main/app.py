0cb4a72abc6bbe19726c984dc2fb3d934e0f0637
marurun66 <marurun66@gmail.com>
2025-04-03 15:02:20 +0900

    Update app.py

--- main/app.py
+++ main/app.py
@@ -4,25 +4,6 @@ from streamlit_option_menu import option_menu
 # 페이지 설정은 항상 먼저
 st.set_page_config(page_icon="🚗", page_title="Hyundai 고객 관리 시스템", layout="wide")
 
-# CSS 스타일 적용
-st.markdown("""
-<style>
-    .main { padding: 2rem; }
-    .sidebar .sidebar-content { padding: 1rem; }
-    div[data-testid="stSidebarUserContent"] { padding: 1rem; }
-    .stButton>button { width: 100%; }
-    .stDownloadButton>button { width: 100%; }
-    /* 컨텐츠 정렬 */
-    .block-container {
-        max-width: 1300px; /* 중앙 정렬을 위한 최대 너비 */
-        margin: auto;
-        padding: 2rem;
-        border-radius: 10px;
-        background-color: white; /* 컨텐츠 부분만 흰색 */
-        box-shadow: 2px 2px 10px rgba(0,0,0,0.1); /* 살짝 그림자 효과 */
-    }
-</style>
-""", unsafe_allow_html=True)
 # 화면 모듈 임포트
 from project_1.ui_mini1.mini1_eda import run_eda
 from project_1.ui_mini1.mini1_home import run_home1
@@ -38,7 +19,7 @@ from project_2.ui_mini2.mini2_home import run_home2
 def main():
     analysis_menu = None 
     with st.sidebar:
-        # ✅ 1. 브랜드 선택 (항상 상단)
+        # ✅ 1. 브랜드 선택 (항상 상단
         brand = option_menu(
             menu_title="브랜드 선택",
             options=["현대", "기아"],
@@ -61,7 +42,7 @@ def main():
             # 국가 선택
             country = st.selectbox(
                 "국가 선택",
-                ["미국", "캐나다", "멕시코","한국"]
+                ["대한민국","미국", "캐나다", "멕시코"]
             )
             st.session_state["country"] = country
 
